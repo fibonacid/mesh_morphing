dd1bea71f52d9604edfdd552cbd06f8008716a34
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-06-06 14:50:49 +0200

    Add scaling function

--- data/shaders/TexLightVert.glsl
+++ data/shaders/TexLightVert.glsl
@@ -35,6 +35,8 @@ uniform vec3 lightFalloff[8];
 uniform vec2 lightSpot[8];
 
 uniform float u_time;
+uniform float u_noise_amount;
+uniform float u_scale;
 
 attribute vec4 position;
 attribute vec4 color;
@@ -170,12 +172,26 @@ float fmb(in vec3 st) {
   return value;
 }
 
+float cycle(float freq) {
+  return 0.5 + 0.5 * sin(freq * u_time * 0.01);
+}
+
+mat4 scale(float amnt) {
+  return mat4(amnt, 0.0, 0.0, 0.0,  // 1. column
+              0.0, amnt, 0.0, 0.0,  // 2. column
+              0.0, 0.0, amnt, 0.0,  // 3. column
+              0.0, 0.0, 0.0, 1.0);  // 4. column
+}
+
 void main() {
 
+  mat4 scaleMatrix = scale(0.5 + 0.5 * u_scale);
+
   float displacement = 100. * (0.5 + 0.5 * fmb(position.xyz * 0.01 + u_time * 0.001));
+  displacement *= u_noise_amount;
 
   // Vertex in clip coordinates
-  gl_Position = transformMatrix * position + displacement;
+  gl_Position = transformMatrix * position * scaleMatrix + vec4(displacement) * scaleMatrix;
 
   // Vertex in eye coordinates
   vec3 ecVertex = vec3(modelviewMatrix * position);
